4681b39d98ddf5a963ec5069c1b01b70c881c066
hdeqi <hdeqi@qq.com>
2020-06-17 16:16:29 +0800

    Vue.prototype plugin tools

--- main.js
+++ main.js
@@ -1,7 +1,19 @@
 import Vue from 'vue'
 import App from './App'
+import * as Api from './config/api.js'
+
+import * as Common from './config/common.js'
+import * as Db from './config/db.js'
+import * as Config from './config/config.js'
+import store from './store'
+import './common/uni-H5Api'
 
 Vue.config.productionTip = false
+Vue.prototype.$api = Api;
+Vue.prototype.$common = Common;
+Vue.prototype.$db = Db;
+Vue.prototype.$config = Config;
+Vue.prototype.$store = store;
 
 App.mpType = 'app'
 
@@ -9,3 +21,4 @@ const app = new Vue({
     ...App
 })
 app.$mount()
+
